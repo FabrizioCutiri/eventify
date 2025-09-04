aa29d86a6e4e6bc19bc631d9e5847a1bc28a91db
Fabrizio <u201914181@upc.edu.pe>
2025-09-03 19:18:09 -0500

    fix(dependencies): fix primeflex and primeicons dependencies in main

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import './style.css'
 import App from './App.vue'
 
 import PrimeVue from 'primevue/config';
-import primeflex from 'primeflex/primeflex.css'
-import primeicons from 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
 import i18n from './i18n/index.js'
 import router from './router/index.js'
 import Material from '@primeuix/themes/material'
